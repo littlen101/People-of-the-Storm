10651c9694a48e213f3f215b07838741a52db9b0
littlen101 <nlewis2016@my.fit.edu>
2018-07-06 14:49:42 -0500

    Update: Build Folder not appearing refreshing repository

--- lib/main.dart
+++ lib/main.dart
@@ -2,16 +2,21 @@ import 'package:flutter/material.dart';
 
 import 'Login.dart';
 
-void main() => runApp(StartUp());
+void main() => runApp(PeopleOfStorm());
 
-class StartUp extends StatelessWidget {
+class PeopleOfStorm extends StatelessWidget {
   @override
   build(BuildContext context) {
     return MaterialApp(
       title: 'People of the Storm',
       theme: ThemeData(
         fontFamily: "Roboto",
-        primarySwatch: Colors.purple[900],
+        primarySwatch: MaterialColor(900, {
+          50: Colors.purple[50],
+          100: Colors.purple[100],
+          200: Colors.purple[200],
+          900: Colors.purple[900]
+        }),
       ),
       home: Login(),
     );
